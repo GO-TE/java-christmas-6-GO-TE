d88b1f9d93dd8a4edf1246936ae5edf1da2f4daa
GO-TE <alsrudxo9810@naver.com>
2023-11-12 00:28:37 +0900

    feat: 에러메세지 상수형 구현

--- src/main/java/christmas/util/message/ErrorMessage.java
+++ src/main/java/christmas/util/message/ErrorMessage.java
@@ -1,2 +1,16 @@
-package christmas.util.message;public enum ErrorMessage {
+package christmas.util.message;
+
+public enum ErrorMessage {
+    NOT_INTEGER_ERROR("[ERROR] 방문 날짜를 숫자로 입력해 주세요!"),
+    INVALID_DATE_ERROR("[ERROR] 방문 날짜를 1~31 사이로 입력해 주세요!");
+
+    final String message;
+
+    ErrorMessage(String message) {
+        this.message = message;
+    }
+
+    public String getMessage() {
+        return message;
+    }
 }
