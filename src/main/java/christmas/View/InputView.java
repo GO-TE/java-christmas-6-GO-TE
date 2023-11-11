3472558d9b321287702b941ca5ce856468be9618
GO-TE <alsrudxo9810@naver.com>
2023-11-11 20:50:22 +0900

    feat(InputVisitDate): 사용자 방문 일자 입력 기능 - 사용자 방문 예정 날짜를 반환한다

--- src/main/java/christmas/View/InputView.java
+++ src/main/java/christmas/View/InputView.java
@@ -1,2 +1,14 @@
-package christmas.View;public class InputView {
+package christmas.View;
+
+import camp.nextstep.edu.missionutils.Console;
+
+public class InputView {
+
+    public int inputVisitDate() { // 메세지 상수화
+        System.out.println("12월 중 식당 예상 방문 날짜는 언제인가요? (숫자만 입력해 주세요!)");
+        return Integer.parseInt(Console.readLine());
+    }
+
+
+
 }
