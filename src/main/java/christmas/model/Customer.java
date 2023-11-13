616bb5a623886da6e0515a4292064dd4d14034d6
GO-TE <alsrudxo9810@naver.com>
2023-11-13 17:26:00 +0900

    feat(Customer): 주문 총 금액 return 메소드 구현

--- src/main/java/christmas/model/Customer.java
+++ src/main/java/christmas/model/Customer.java
@@ -1,2 +1,26 @@
-package christmas.model;public class Customer {
+package christmas.model;
+
+import java.util.HashMap;
+import christmas.util.menu.Menu;
+public class Customer {
+    private final int visitDate;
+    private final HashMap<String, Integer> order;
+
+    public Customer(int visitDate, HashMap<String, Integer> order) {
+        this.visitDate = visitDate;
+        this.order = order;
+    }
+
+    public int getTotalPrice() {
+        int totalPrice = 0;
+        HashMap<String, Integer> menu = new Menu().getMenuMap();
+        for (String menuName :order.keySet()) {
+            totalPrice += menu.get(menuName) * order.get(menuName);
+        }
+        return totalPrice;
+    }
+
+
+
+
 }
