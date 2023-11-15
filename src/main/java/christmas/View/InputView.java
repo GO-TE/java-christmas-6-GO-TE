55d69f4f684dcef1900a7049a659665fa3b2b30e
GO-TE <alsrudxo9810@naver.com>
2023-11-15 13:29:44 +0900

    refactor(InputView): 예외 발생시 다시 받도록 수정

--- src/main/java/christmas/View/InputView.java
+++ src/main/java/christmas/View/InputView.java
@@ -1,8 +1,8 @@
 package christmas.View;
 
 import camp.nextstep.edu.missionutils.Console;
-import christmas.util.DateValidator;
-import christmas.util.OrderValidator;
+import christmas.util.validator.DateValidator;
+import christmas.util.validator.OrderValidator;
 import java.util.HashMap;
 
 
@@ -13,7 +13,16 @@ public class InputView {
     public int inputVisitDate() { // 메세지 상수화
         System.out.println("12월 중 식당 예상 방문 날짜는 언제인가요? (숫자만 입력해 주세요!)");
         String input = Console.readLine();
-        dateValidator.validateDate(input);
+        while (true) {
+            try {
+                dateValidator.validateDate(input);
+                break;
+            } catch (IllegalArgumentException e) {
+                System.out.println(e.getMessage());
+                input = Console.readLine();
+            }
+        }
+
         return Integer.parseInt(input);
 
     }
@@ -21,7 +30,15 @@ public class InputView {
     public HashMap<String, Integer> inputOrderMenu() {
         System.out.println("주문하실 메뉴를 메뉴와 개수를 알려주세요. (e.g. 해산물파스타-2,레드와인-1,초코케이크-1");
         String input = Console.readLine();
-        return orderValidator.validateOrderMenu(input);
+
+        while(true) {
+            try {
+                return orderValidator.validateOrderMenu(input);
+            } catch (IllegalArgumentException e) {
+                System.out.println(e.getMessage());
+                input = Console.readLine();
+            }
+        }
     }
 
 }
