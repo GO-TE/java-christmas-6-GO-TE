34be8986c429af07fab3b48fd14f00ec816b6c69
GO-TE <alsrudxo9810@naver.com>
2023-11-15 11:29:31 +0900

    feat(Event): 주문에 따른 이벤트 기능 구현

--- src/main/java/christmas/model/Event.java
+++ src/main/java/christmas/model/Event.java
@@ -1,2 +1,92 @@
-package christmas.model;public class Event {
+package christmas.model;
+
+import christmas.util.Batch;
+import christmas.util.Constant;
+import christmas.util.menu.Drink;
+import christmas.util.message.EventListMessage;
+import java.util.HashMap;
+
+
+public class Event {
+    int christmasDDay = 0;
+    int starDay = 0;
+    boolean weekend = false;
+    boolean champagneEvent;
+    int dayDiscountEvent;
+    String batch;
+
+    public Event(int date, int totalPrice, int count) {
+        if (date <= Constant.CHRISTMAS) {
+            christmasDDay = Constant.DISCOUNT_BY_DAY * date + Constant.BASE_DISCOUNT;
+        }
+        if (date % Constant.WEEK == Constant.STAR_DAY || date == Constant.CHRISTMAS) {
+            starDay = Constant.BASE_DISCOUNT;
+        }
+        if (Constant.WEEKEND.contains(date % Constant.WEEK)) {
+            weekend = true;
+        }
+        champagneEvent = (totalPrice >= Constant.CHAMPAGNE_EVENT);
+        dayDiscountEvent = setDayDiscountEvent(count);
+        batch = setBatch(getTotalBenefit());
+    }
+
+    public Event() {
+        christmasDDay = 0;
+        starDay = 0;
+        champagneEvent = false;
+        dayDiscountEvent = 0;
+    }
+
+    public int setDayDiscountEvent(int count) {
+        return count * Constant.MENU_DISCOUNT;
+    }
+
+    public int getTotalBenefit() {
+        int totalBenefit = 0;
+        if (champagneEvent) {
+            totalBenefit += Drink.CHAMPAGNE.getPrice();
+        }
+        totalBenefit += getTotalDiscount();
+        return totalBenefit;
+    }
+
+    public int getTotalDiscount() {
+        return christmasDDay + starDay + dayDiscountEvent;
+    }
+
+    public String setBatch(int totalBenefit) {
+        for (Batch eachBatch : Batch.values()) {
+            if (eachBatch.getCurrentBatch(totalBenefit)) {
+                return eachBatch.getBatchName();
+            }
+        }
+        return Constant.NOTHING;
+    }
+
+    public boolean isChampagneEvent() {
+        return champagneEvent;
+    }
+
+    public HashMap<String, Integer> getEventList(int count) {
+        HashMap<String, Integer> eventList = new HashMap<>();
+        eventList.put(EventListMessage.CHRISTMAS_D_DAY.getEvent(), christmasDDay);
+        if (weekend) {
+            eventList.put(EventListMessage.WEEKEND.getEvent(), dayDiscountEvent);
+        }
+        if (!weekend) {
+            eventList.put(EventListMessage.WEEKDAY.getEvent(), dayDiscountEvent);
+        }
+        eventList.put(EventListMessage.STAR_DAY.getEvent(), starDay);
+        if (champagneEvent) {
+            eventList.put(EventListMessage.CHAMPAGNE.getEvent(), Drink.CHAMPAGNE.getPrice());
+        }
+        if (!champagneEvent) {
+            eventList.put(EventListMessage.CHAMPAGNE.getEvent(), 0);
+        }
+        return eventList;
+    }
+
+    public String getBatch() {
+        return batch;
+    }
 }
