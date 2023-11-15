20732a904abdcb7cd8bd6cab17f4eadb7d3dee0f
GO-TE <alsrudxo9810@naver.com>
2023-11-15 13:33:58 +0900

    feat(Customer): 특정 메뉴 갯수 반환 기능 구현 getDesertCount() : - 주문 목록 중 디저트 메뉴 갯수 반환 getMainMenuCount() : - 주문 목록 중 메인 메뉴 갯수 반환

--- src/main/java/christmas/model/Customer.java
+++ src/main/java/christmas/model/Customer.java
@@ -1,7 +1,12 @@
 package christmas.model;
 
+import christmas.util.menu.Food;
+import christmas.util.menu.MainMenu;
+import java.util.ArrayList;
 import java.util.HashMap;
 import christmas.util.menu.Menu;
+import java.util.List;
+
 public class Customer {
     private final int visitDate;
     private final HashMap<String, Integer> order;
@@ -14,13 +19,41 @@ public class Customer {
     public int getTotalPrice() {
         int totalPrice = 0;
         HashMap<String, Integer> menu = new Menu().getMenuMap();
-        for (String menuName :order.keySet()) {
+        for (String menuName : order.keySet()) {
             totalPrice += menu.get(menuName) * order.get(menuName);
         }
         return totalPrice;
     }
 
+    public boolean checkEventRequirement() {
+        return getTotalPrice() >= 10000;
+    }
 
+    public int getVisitDate() {
+        return visitDate;
+    }
 
+    public int getDesertCount() {
+        int count = 0;
+        for (String menu : order.keySet()) {
+            if (new Menu().getMainDesertName().contains(menu)) {
+                count += order.get(menu);
+            }
+        }
+        return count;
+    }
 
+    public int getMainMenuCount() {
+        int count = 0;
+        for (String menu : order.keySet()) {
+            if (new Menu().getMainMenuName().contains(menu)) {
+                count += order.get(menu);
+            }
+        }
+        return count;
+    }
+
+    public HashMap<String, Integer> getOrder() {
+        return order;
+    }
 }
